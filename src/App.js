5bdc39c5c5207c9f1950701db1279a6e314af9d4
Ryan Howard <ryanghoward@protonmail.com>
2022-12-12 20:22:57 -0800

    buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 function App() {
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(<button key={i}>{i}</button>);
+    }
+    return digits;
+  };
+
   return (
     <div className='App'>
-      <h1>Reactulator</h1>
+      <div className='calculator'>
+        <div className='display'>
+          <span>(0)</span> 0
+        </div>
+        <div className='operators'>
+          <button>÷</button>
+          <button>x</button>
+          <button>+</button>
+          <button>-</button>
+          <button>Delete</button>
+        </div>
+        <div className='digits'>
+          {createDigits()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
