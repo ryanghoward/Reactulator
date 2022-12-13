04fa7529b19beeccd010d1642e36102f219a253c
Ryan Howard <ryanghoward@protonmail.com>
2022-12-12 20:47:16 -0800

    buttons workin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["÷", "x", "+", "-", "."];
+
+  const updateCalc = (value) => {
+    setCalc(calc + value);
+  };
+
   const createDigits = () => {
     const digits = [];
     for (let i = 1; i < 10; i++) {
-      digits.push(<button key={i}>{i}</button>);
+      digits.push(
+        <button onClick={() => updateCalc(i.toString())} key={i}>
+          {i}
+        </button>
+      );
     }
     return digits;
   };
@@ -11,20 +26,21 @@ function App() {
     <div className='App'>
       <div className='calculator'>
         <div className='display'>
-          <span>(0)</span> 0
+          {result ? <span>(0)</span> : ""}
+          {calc || "0"}
         </div>
         <div className='operators'>
-          <button>÷</button>
-          <button>x</button>
-          <button>+</button>
-          <button>-</button>
-          <button>Delete</button>
+          <button onClick={() => updateCalc("÷")}>÷</button>
+          <button onClick={() => updateCalc("x")}>x</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+          <button>DEL</button>
         </div>
         <div className='digits'>
           {createDigits()}
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+          <button onClick={() => updateCalc("=")}>=</button>
         </div>
       </div>
     </div>
